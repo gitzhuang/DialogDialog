48bf5c7d3d10365029bde2fd20da3aee84299cd0
luoqqsh <luoqqsh@163.com>
2018-07-05 20:57:58 +0800

    代码结构优化

---
+++ app/src/main/java/x/com/dialogdialog/CheckNotificationActivity.java
@@ -0,0 +1,71 @@
+package x.com.dialogdialog;
+
+import android.app.Notification;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import x.com.dialogmobile.CheckUpdate.CheckDialogFragment;
+import x.com.dialogmobile.NotificationHelper;
+
+public class CheckNotificationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new CheckDialogFragment(this,
+                "版本更新内容",
+                0,
+                "http://imtt.dd.qq.com/16891/337A49BBE7A8A0B42E2312893903BBB3.apk?fsname=com.coolsnow.screenshot_5.6.0_56000.apk",
+//                "http://imtt.dd.qq.com/16891/1F9DFAAC8C158F24D5A320A044AD352A.apk?fsname=com.qiyi.video_9.6.5_81100.apk",
+                0,
+                R.mipmap.icon_error,
+                "下载通知标题",
+                new CheckDialogFragment.OnCheckcallback() {
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(CheckNotificationActivity.this, "345", Toast.LENGTH_SHORT).show();
+                    }
+                }).show(getSupportFragmentManager(), "checkup");
+
+//        //优先级default（3）
+//        new NotificationHelper(this, "默认通知")
+//                .setContent("测试1")//通知内容
+//                .setDefaults(Notification.DEFAULT_ALL)//设置提醒方式
+//                .setType(NotificationHelper.NOTIFICATION_TYPE_NORMAL)//通知类型
+//                .setSmallIcon(R.mipmap.ic_launcher)//
+//                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.icon_error))
+//                .setContextIntent(new Intent(Intent.ACTION_SEND))//设置跳转
+//                .notifyShow();
+//
+//        //优先级min（1）
+//        new NotificationHelper(this, "其他")
+//                .setContent("测试1")//通知内容
+//                .setDefaults(Notification.DEFAULT_ALL)//设置提醒方式
+//                .setType(NotificationHelper.NOTIFICATION_TYPE_OTHER)//通知类型
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.icon_error))
+//                .setContextIntent(new Intent(Intent.ACTION_SEND))
+//                .notifyShow();
+//
+//        //优先级low（2）
+        NotificationHelper notificationHelper = new NotificationHelper(this, "下载通知")
+                .setContent("测试1")//通知内容
+                .setDefaults(Notification.DEFAULT_ALL)//设置提醒方式
+                .setType(NotificationHelper.NOTIFICATION_TYPE_DOWNLOAD)//通知类型
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.icon_error));
+        notificationHelper.setProgress(50, new Intent(Intent.ACTION_SEND));
+        notificationHelper.notifyShow();
+
+
+        //取消当前通知
+//        notificationHelper.cancel();
+        //取消全部通知
+//        NotificationHelper.cancelAll(this);
+
+    }
+
+}
